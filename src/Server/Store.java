3d16352160cd29cd24090d388e2cfb25ebb159e4
HuangFF <625173@qq.com>
2022-11-22 01:54:21 +0800

    优化checkstyle

--- src/Server/Store.java
+++ src/Server/Store.java
@@ -1,13 +1,13 @@
 package Server;
 
 public class Store {
-    //A private volatile String to store the game state
-    public volatile String strGameState;
+  //A private volatile String to store the game state
+  public volatile String strGameState;
 
-public  String state="waiting....";
-public int round=0;
-    public  int step=0;
-    public  boolean player1On=true;
-    public  boolean player2On=true;
-    public boolean serverOn=true;
+  public String state = "waiting....";
+  public int round = 0;
+  public int step = 0;
+  public boolean player1On = true;
+  public boolean player2On = true;
+  public boolean serverOn = true;
 }
